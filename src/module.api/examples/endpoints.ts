17958b93ad22fd2d1715835b6abdcb4de8c9b86a
Tiago Santos <tiagosn03@gmail.com>
2019-11-08 11:31:51 -0300

    Rename and update some files in the API calls examples folder.

--- src/module.api/examples/endpoints.ts
+++ src/module.api/examples/endpoints.ts
@@ -1,10 +1,15 @@
-const search_movie =
+const search_movie_by_name =
   "https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher";
+
+const by_genre =
+  "https://api.themoviedb.org/3/discover/movie?api_key=d884e331c73a682a488cf33f32f919ed&sort_by=popularity.desc&page=1&with_genres=99";
+// organizado pro popularidade
+// gênero: documentário, id: 99
 
 const by_id = "https://api.themoviedb.org/3/movie/76341?api_key=<<api_key>>";
 
 const configuration =
   "https://api.themoviedb.org/3/configuration?api_key=<<api_key>>";
 
-const genres =
+const genre_list =
   "https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>";
