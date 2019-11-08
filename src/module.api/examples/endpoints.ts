4fb93462a2c847788e58d88b34d4d0bf72e09071
Tiago Santos <tiagosn03@gmail.com>
2019-11-08 10:58:23 -0300

    Rename an existente example file and add a new one.

--- src/module.api/examples/endpoints.ts
+++ src/module.api/examples/endpoints.ts
@@ -1,7 +1,10 @@
-const search =
+const search_movie =
   "https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher";
 
 const by_id = "https://api.themoviedb.org/3/movie/76341?api_key=<<api_key>>";
 
 const configuration =
   "https://api.themoviedb.org/3/configuration?api_key=<<api_key>>";
+
+const genres =
+  "https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>";
