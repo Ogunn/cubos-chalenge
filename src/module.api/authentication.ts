4f133e5f10c7628f63d22e1d36886b33dad6fb0c
Tiago Santos <tiagosn03@gmail.com>
2019-11-09 19:34:21 -0300

    bug fix: prefix the environment variable name with an REACT_API.

--- src/module.api/authentication.ts
+++ src/module.api/authentication.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const loadAPIKey = () => {
-  if (process.env.API_KEY) return process.env.API_KEY;
+  if (process.env.REACT_APP_API_KEY) return process.env.REACT_APP_API_KEY;
   else {
     console.log("error: API_KEY variable environment is not set");
     return "";
