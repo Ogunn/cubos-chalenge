e60897a0cff34acec4c5a0f6f603b09665357332
Tiago Santos <tiagosn03@gmail.com>
2019-11-09 19:32:54 -0300

    Add MDBApi types.

--- src/module.api/interfaces/MovieDBInterfaces.ts
+++ src/module.api/interfaces/MovieDBInterfaces.ts
@@ -2,3 +2,27 @@ export interface MDBMovieGenre {
   id: number;
   name: string;
 }
+
+export interface MDBResponse {
+  page: number;
+  total_results: number;
+  total_pages: number;
+  results: Result[];
+}
+
+export interface Result {
+  popularity: number;
+  id: number;
+  video: boolean;
+  vote_count: number;
+  vote_average: number;
+  title: string;
+  release_date: Date;
+  original_language: string;
+  original_title: string;
+  genre_ids: number[];
+  backdrop_path: string;
+  adult: boolean;
+  overview: string;
+  poster_path: string;
+}
