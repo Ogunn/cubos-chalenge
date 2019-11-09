92df977573a16419b0dd7384f4bb7cf0edb7b14b
Tiago Santos <tiagosn03@gmail.com>
2019-11-09 19:35:00 -0300

    Fetch example data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,43 @@
-import * as React from "react";
-import { useState } from "react";
+import * as React from 'react';
+import axios from 'axios';
+
+import buildSearchByNameOrSearchByGenreURL from './module.api/MovieDbApiUrlBuilder';
+import { MDBResponse } from './module.api/interfaces/MovieDBInterfaces';
 
 const App: React.FC = () => {
-  const [query, setQuery] = useState<string>();
+  const [inputValue, setInputValue] = React.useState<string>('us');
+  const [data, setData] = React.useState<MDBResponse>();
 
-  const fetchData = () => {
-    console.log(query);
-  };
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const apiKey = process.env.REACT_APP_API_KEY;
+      let url: string;
+      if (apiKey) {
+        url = buildSearchByNameOrSearchByGenreURL(inputValue, apiKey);
+        const result = await axios(url);
+
+        setData(result.data);
+      }
+    }
+
+    fetchData();
+  }, [inputValue]);
 
   return (
     <div className="App">
       <form
-        onSubmit={e => {
+        onSubmit={(e) => {
           e.preventDefault();
-          fetchData();
         }}
       >
         <input
           type="text"
           placeholder="Busque um filme por nome, ano ou gênero."
-          onChange={event => setQuery(event.target.value)}
+          onChange={(event) => setInputValue(event.target.value)}
         />
       </form>
+      <h2>{data && data.results[0].original_title}</h2>
+      <p>{data && data.results[0].overview}</p>
     </div>
   );
 };
