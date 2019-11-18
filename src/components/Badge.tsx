0844f49a92693336747013b3fb0d2f867546dca8
Tiago Santos <tiagosn03@gmail.com>
2019-11-18 18:24:55 -0300

    Add Styled-component.

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
-import "./Badge.css";
+import styled from "styled-components";
 
 interface IBadgeProps {
   text: string;
 }
 
+const Span = styled.span`
+  border: 2px solid #116193;
+  padding: 2px;
+  border-radius: 20px;
+  background-color: white;
+`;
+
 const Badge: React.FC<IBadgeProps> = ({ text }) => {
-  return <span className="badge">{text}</span>;
+  return <Span className="badge">{text}</Span>;
 };
 
 export default Badge;
