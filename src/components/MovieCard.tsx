5176013a4f9bcbec57984f824c46d10eb9732322
Tiago Santos <tiagosn03@gmail.com>
2019-11-19 16:53:09 -0300

    Complement last commit: Fix CardContent height problem.

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,21 +1,45 @@
 import * as React from "react";
+import styled from "styled-components";
 import { Result as Movie } from "../module.api/interfaces/MovieDBInterfaces";
 import Banner from "./Banner";
 import CardContent from "./CardContent";
-
-import "./MovieCard.css";
 
 interface IMovieCardProps {
   movie: Movie;
   posterUrl: string;
 }
 
+const Div = styled.div`
+  background-color: #ebebeb;
+  margin-bottom: 15px;
+
+  @media only screen and (min-width: 375px) {
+  }
+  @media only screen and (min-width: 425px) {
+  }
+  @media only screen and (min-width: 768px) {
+  }
+  @media only screen and (min-width: 1024px) {
+  }
+  @media only screen and (min-width: 1440px) {
+  }
+  @media only screen and (min-width: 2560px) {
+  }
+`;
+
 const MovieCard: React.FC<IMovieCardProps> = ({ movie, posterUrl }) => {
+  const [bannerHeight, setBannerHeight] = React.useState();
+
   return (
-    <div className="movie-card flex" key={movie.id}>
-      <Banner ImageUrl={posterUrl} />
-      <CardContent movie={movie} />
-    </div>
+    <Div className="movie-card flex" key={movie.id}>
+      <Banner
+        imageUrl={posterUrl}
+        imageOffsetHeight={offsetHeight => {
+          setBannerHeight(offsetHeight);
+        }}
+      />
+      <CardContent movie={movie} height={bannerHeight} />
+    </Div>
   );
 };
 
