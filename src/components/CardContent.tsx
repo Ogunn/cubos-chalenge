19f305be608691c5208d59a49df91edecc324ac3
Tiago Santos <tiagosn03@gmail.com>
2019-11-26 08:10:17 -0300

    Make CardContent a styled component.

--- src/components/CardContent.tsx
+++ src/components/CardContent.tsx
@@ -1,15 +1,15 @@
-import * as React from "react";
+import React from "react";
 import styled from "styled-components";
+
 import { Result as Movie } from "../module.api/interfaces/MovieDBInterfaces";
+
 import BadgeGroup from "./BadgeGroup";
 import RateBadge from "./RateBadge";
 import CardTitle from "./CardTitle";
 
-import "./CardContent.css";
-
 interface ICardContentProps {
   movie: Movie;
-  height: number; // set movie-info height
+  height: number; // Set movie-info height: maybe this prop could be optional.
 }
 
 const Div = styled.div<{ height: number }>`
@@ -19,19 +19,59 @@ const Div = styled.div<{ height: number }>`
   grid-template-rows: 1fr 8fr;
 `;
 
+const Sinopse = styled.div`
+  position: relative;
+
+  .release-date {
+    font-family: "Abel", sans-serif;
+    font-size: 0.6rem;
+    margin-left: 3px;
+    position: absolute;
+    top: 0;
+
+    @media only screen and (min-width: 480px) {
+      font-size: 0.8rem;
+    }
+
+    @media only screen and (min-width: 768px) {
+      font-size: 0.9rem;
+    }
+  }
+
+  p {
+    font-size: 0.7rem;
+    position: absolute;
+    top: 1.3rem;
+    overflow: hidden;
+    height: 55px;
+    margin-left: 15px;
+    margin-right: 5px;
+
+    @media only screen and (min-width: 480px) {
+      height: 85px;
+      font-size: 1rem;
+    }
+
+    @media only screen and (min-width: 768px) {
+      height: 148px;
+      font-size: 1.2rem;
+    }
+  }
+`;
+
 const CardContent: React.FC<ICardContentProps> = ({ movie, height }) => {
   return (
     <Div height={height}>
       <CardTitle text={movie.original_title} />
 
-      <div className="sinopse">
+      <Sinopse className="sinopse">
         <RateBadge rate={movie.vote_average * 10} />
 
         <span className="release-date">{movie.release_date}</span>
 
         <p className="margin-0 font-lato">{movie.overview}</p>
         <BadgeGroup genreIdList={movie.genre_ids} />
-      </div>
+      </Sinopse>
     </Div>
   );
 };
