e48cc307f28a17d1e917763863810cda28d208cc
Tiago <tiagoletras45@gmail.com>
2019-12-27 08:53:13 -0300

    Add search movie types.

--- src/service/api/types.ts
+++ src/service/api/types.ts
@@ -26,3 +26,27 @@ export interface Result {
   overview: string;
   poster_path: string;
 }
+
+export interface SearchMovieResponse {
+  page: number;
+  total_results: number;
+  total_pages: number;
+  results: SearchMovieResult[];
+}
+
+export interface SearchMovieResult {
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  poster_path: string;
+  id: number;
+  adult: boolean;
+  backdrop_path: string;
+  original_language: string;
+  original_title: string;
+  genre_ids: number[];
+  title: string;
+  vote_average: number;
+  overview: string;
+  release_date: Date;
+}
