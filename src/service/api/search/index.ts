bc6ca166c8b44dddd5977120fa59aede13d57135
Tiago <tiagoletras45@gmail.com>
2019-12-31 10:49:57 -0300

    Move service api search to its own folder.

---
+++ src/service/api/search/index.ts
@@ -0,0 +1,22 @@
+import { Response } from './types';
+import axios from '../axios';
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+export const movie = async (query: string) => {
+  try {
+    const response = await axios.get<Response>('/search/movie', {
+      params: {
+        api_key: apiKey,
+        query
+      }
+    });
+
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+
+    return null;
+  }
+};
