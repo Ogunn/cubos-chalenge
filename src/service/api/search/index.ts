1614dc23e400597610f687d0f37e4da156f90333
Tiago <tiagoletras45@gmail.com>
2019-12-31 19:23:39 -0300

    Rename interface.

--- src/service/api/search/index.ts
+++ src/service/api/search/index.ts
@@ -1,11 +1,11 @@
-import { Response } from './types';
+import { MovieResponse } from './types';
 import axios from '../axios';
 
 const apiKey = process.env.REACT_APP_API_KEY;
 
 export const movie = async (query: string) => {
   try {
-    const response = await axios.get<Response>('/search/movie', {
+    const response = await axios.get<MovieResponse>('/search/movie', {
       params: {
         api_key: apiKey,
         query
