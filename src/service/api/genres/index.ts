00b6620debb279e4f8fa7816b6962162c0035528
Tiago <tiagoletras45@gmail.com>
2019-12-31 19:09:48 -0300

    Make getMovieList() async.

--- src/service/api/genres/index.ts
+++ src/service/api/genres/index.ts
@@ -1,8 +1,8 @@
-import { Response } from './types';
+import { GetListResponse } from './types';
 // import { get } from '../getData';
 import { get } from '../axios';
 
-export const getMovieList = (language?: string) =>
-  get<Response>('/genre/movie/list', {
+export const getMovieList = async (language?: string) =>
+  await get<GetListResponse>('/genre/movie/list', {
     language
   });
