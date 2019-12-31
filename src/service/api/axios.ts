fff5b29dd1d4f14ab996718d6d1d401eb97b9cfa
Tiago <tiagoletras45@gmail.com>
2019-12-31 08:49:01 -0300

    Create a generic get request that uses the axios instance.

--- src/service/api/axios.ts
+++ src/service/api/axios.ts
@@ -1,7 +1,33 @@
 import axios from 'axios';
 
+const apiKey = process.env.REACT_APP_API_KEY;
+
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3'
 });
+
+/**
+ * A generic get request using axios instance.
+ * This method uses the api kay placed in the .env file.
+ * @param url - the same url string used in axios.get()
+ * @param params - the same params object used in axios.get()
+ */
+export const get = async <T>(url: string, params?: any) => {
+  try {
+    const response = await instance.get<T>(url, {
+      params: {
+        api_key: apiKey,
+        ...params
+      }
+    });
+
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+
+    return null;
+  }
+};
 
 export default instance;
