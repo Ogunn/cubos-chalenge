6ec26663e4aaf2f9551a23633e75ad630afb71a8
Tiago <tiagoletras45@gmail.com>
2020-01-02 23:46:57 -0300

    <App />: clean.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 
 import api from './service/api';
 import { Movie } from './service/api/search/types';
 
-import MovieCard from './components/MovieCard';
 import Header from './components/Header/Header';
 import Form from './components/Form';
+import MovieList from './components/MovieList';
 
 const App: React.FC = () => {
   const [inputValue, setInputValue] = React.useState<string>('');
   const [query, setQuery] = React.useState<string>('black panther');
-  const [movieList, setMovieList] = React.useState<Movie[]>();
-
+  const [movieList, setMovieList] = React.useState<Movie[] | null>(null);
   const baseImageUrl = 'https://image.tmdb.org/t/p/w300'; // It's important get this string dinamicaly because it may change over time.
 
-  React.useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
       const movieListResponse = await api.search.movie(query);
 
@@ -26,27 +25,24 @@ const App: React.FC = () => {
     fetchData();
   }, [query]);
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setInputValue(e.currentTarget.value);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setQuery(inputValue);
+  };
+
   return (
     <div>
       <Header text="Movies" />
-
       <main>
         <Form
-          handleSubmit={e => {
-            e.preventDefault();
-            setQuery(inputValue);
-          }}
-          handleInputChange={e => setInputValue(e.target.value)}
+          handleSubmit={handleSubmit}
+          handleInputChange={handleInputChange}
           placeHolder="Busque um filme por nome ou por gênero."
         />
-        {movieList &&
-          movieList.map(movie => (
-            <MovieCard
-              movie={movie}
-              posterUrl={baseImageUrl + movie.poster_path}
-              key={movie.id}
-            />
-          ))}
+        <MovieList movieList={movieList} baseUrl={baseImageUrl} />
       </main>
     </div>
   );
